d221db9d6a762c8d87194bcff50a60cc88eb32f4
AG-alexander <you@example.com>
2019-09-14 17:17:33 -0600

    api mensaje

--- twitter-backend/application/controllers/api/Login.php
+++ twitter-backend/application/controllers/api/Login.php
@@ -15,7 +15,7 @@ class Login extends REST_Controller {
         if(!$data) {
             $data = "Usuaio o contraseña incorrectos";
         } else {
-            $this->db->get_where('tb_usuario', ['user' => $data['user'], 'password' => $data['password']])->row_array();
+            $data = $this->db->get_where('tb_usuario', ['user' => $data['user'], 'password' => $data['password']])->row_array();
         }
         $this->response($data, REST_Controller::HTTP_OK);
     }
--- twitter-backend/application/controllers/api/message.php
+++ twitter-backend/application/controllers/api/message.php
@@ -6,18 +6,42 @@ require APPPATH . 'libraries/REST_Controller.php';
 class Message extends REST_Controller {
     public function __construct(){
         parent::__construct();
-        $this->load->database()
+        $this->load->database();
     }
 
-    public function authenticationUser() {
+    public function getMessage_get() {
+        $id = $this->uri->segment(4);
+
+        if(!empty($id)){
+            $data = $this->db->get_where('tb_mensaje', ['id' => $id])->row_array();
+        }else{
+            $data = $this->db->get('tb_mensaje')->result();
+        }
+        if(!$data)
+            $data = 'No hay registros con este ID.';
+        $this->response($data, REST_Controller::HTTP_OK);
         
+    }
+
+    public function postMessage_post() {
         $data = $this->input->post();
 
-        if(!$data) {
-            $data = "Usuaio o contraseña incorrectos";
-        } else {
-            $data = $this->db->get_where('tbusuario', ['user' => $data['user'], 'password' => $data['password']])->row_array();
-        }
+        if($this->db->insert('tb_mensaje', $data))
+        $this->response('Item creado con éxito.', REST_Controller::HTTP_OK); 
     }
+
+    public function updateMessage_put() {
+        $id = $this->put('id');
+        $data = $this->put();
+
+        if($this->db->update('tb_mensaje', $data, array('id'=>$id)))
+            $this->response('Item actualizado con éxito.', REST_Controller::HTTP_OK);
+    }
+
+    public function deleteNews_get() {
+            $id = $this->uri->segment(4);
+
+            if($this->db->delete('tb_mensaje', array('id'=>$id)))
+                $this->response('Item eliminado con éxito.', REST_Controller::HTTP_OK);
+        }
 }
->
